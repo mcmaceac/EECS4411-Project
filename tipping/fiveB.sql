a9b5dc939b41a0089b2bf5071c8c3a42e0cabc74
mjce <ce.alterego@gmail.com>
2018-04-16 21:52:54 -0400

    Update fiveB.sql

--- tipping/fiveB.sql
+++ tipping/fiveB.sql
@@ -1,4 +1,4 @@
-select *
-  from stl.purchase p
-  where b.book# = p.book# and p.when between '10/01/2001' and
-          '10/31/2010'
+select avg(sale)
+  from stl.purchase p, stl.book b
+  where b.book# =p.book# and p.qnty > 100
+  group by genre
